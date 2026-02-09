b7fac2b0cfd40dce6be3bf879d52e2b23c5bb6d0
olewandowski1 <olewandowski@soldevelo.com>
2026-02-09 10:09:10 +0100

    fix: adjust filters spacing

--- src/components/PolicyHolderFilter.jsx
+++ src/components/PolicyHolderFilter.jsx
@@ -1,141 +1,173 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 import { injectIntl } from 'react-intl';
-import { styled } from "@mui/material/styles";
-import { withModulesManager, formatMessage, TextInput, PublishedComponent } from "@openimis/fe-core";
-import { Grid, FormControlLabel, Checkbox } from "@mui/material";
-import { GREATER_OR_EQUAL_LOOKUP, LESS_OR_EQUAL_LOOKUP, DATE_TO_DATETIME_SUFFIX, CONTAINS_LOOKUP } from "../constants"
+import { styled } from '@mui/material/styles';
+import {
+  withModulesManager,
+  formatMessage,
+  TextInput,
+  PublishedComponent,
+} from '@openimis/fe-core';
+import { Grid, FormControlLabel, Checkbox } from '@mui/material';
+import {
+  GREATER_OR_EQUAL_LOOKUP,
+  LESS_OR_EQUAL_LOOKUP,
+  DATE_TO_DATETIME_SUFFIX,
+  CONTAINS_LOOKUP,
+} from '../constants';
 
 const StyledForm = styled('div')(({ theme }) => ({
-  padding: 0
+  padding: 0,
+  '& .locationWrapper': {
+    paddingTop: theme?.spacing ? theme.spacing(1) : 8,
+    paddingLeft: theme?.spacing ? theme.spacing(1) : 8,
+    paddingRight: theme?.spacing ? theme.spacing(1) : 8,
+  },
 }));
 
 const StyledItem = styled('div')(({ theme }) => ({
-  padding: theme?.spacing?.(1)
+  padding: theme?.spacing?.(1),
 }));
 
 class PolicyHolderFilter extends Component {
-    _filterValue = k => {
-        const { filters } = this.props;
-        return !!filters[k] ? filters[k].value : null;
-    }
+  _filterValue = (k) => {
+    const { filters } = this.props;
+    return !!filters[k] ? filters[k].value : null;
+  };
 
-    _filterTextFieldValue = (key) => {
-        const { filters } = this.props;
-        return !!filters[key] ? filters[key].value : "";
-    }
+  _filterTextFieldValue = (key) => {
+    const { filters } = this.props;
+    return !!filters[key] ? filters[key].value : '';
+  };
 
-    _onChangeFilter = (k, v) => {
-        this.props.onChangeFilters([
-            {
-                id: k,
-                value: v,
-                filter: `${k}: ${v}`
+  _onChangeFilter = (k, v) => {
+    this.props.onChangeFilters([
+      {
+        id: k,
+        value: v,
+        filter: `${k}: ${v}`,
+      },
+    ]);
+  };
+
+  _onChangeStringFilter = (k, v, lookup) => {
+    this.props.onChangeFilters([
+      {
+        id: k,
+        value: v,
+        filter: `${k}_${lookup}: "${v}"`,
+      },
+    ]);
+  };
+
+  _onChangeDateFilter = (k, v, lookup) => {
+    this.props.onChangeFilters([
+      {
+        id: k,
+        value: v,
+        filter: `${k}_${lookup}: "${v}${DATE_TO_DATETIME_SUFFIX}"`,
+      },
+    ]);
+  };
+
+  render() {
+    const { intl, filters, onChangeFilters } = this.props;
+    return (
+      <Grid container component={StyledForm}>
+        <Grid size={12} className='locationWrapper'>
+          <PublishedComponent
+            pubRef='location.DetailedLocationFilter'
+            withNull={true}
+            filters={filters}
+            onChangeFilters={onChangeFilters}
+            anchor='parentLocation'
+            split
+          />
+        </Grid>
+        <Grid size={3} component={StyledItem}>
+          <TextInput
+            module='policyHolder'
+            label='code'
+            value={this._filterTextFieldValue('code')}
+            onChange={(v) =>
+              this._onChangeStringFilter('code', v, CONTAINS_LOOKUP)
             }
-        ])
-    }
-
-    _onChangeStringFilter = (k, v, lookup) => {
-        this.props.onChangeFilters([
-            {
-                id: k,
-                value: v,
-                filter: `${k}_${lookup}: "${v}"`
+          />
+        </Grid>
+        <Grid size={3} component={StyledItem}>
+          <TextInput
+            module='policyHolder'
+            label='tradeName'
+            value={this._filterTextFieldValue('tradeName')}
+            onChange={(v) =>
+              this._onChangeStringFilter('tradeName', v, CONTAINS_LOOKUP)
             }
-        ])
-    }
-
-    _onChangeDateFilter = (k, v, lookup) => {
-        this.props.onChangeFilters([
-            {
-                id: k,
-                value: v,
-                filter: `${k}_${lookup}: "${v}${DATE_TO_DATETIME_SUFFIX}"`
+          />
+        </Grid>
+        <Grid size={3} component={StyledItem}>
+          <PublishedComponent
+            pubRef='policyHolder.LegalFormPicker'
+            module='policyHolder'
+            label='legalForm'
+            withNull
+            nullLabel={formatMessage(intl, 'policyHolder', 'any')}
+            value={this._filterValue('legalForm')}
+            onChange={(v) => this._onChangeFilter('legalForm', v)}
+          />
+        </Grid>
+        <Grid size={3} component={StyledItem}>
+          <PublishedComponent
+            pubRef='policyHolder.ActivityCodePicker'
+            module='policyHolder'
+            label='activityCode'
+            withNull
+            nullLabel={formatMessage(intl, 'policyHolder', 'any')}
+            value={this._filterValue('activityCode')}
+            onChange={(v) => this._onChangeFilter('activityCode', v)}
+          />
+        </Grid>
+        <Grid size={3} component={StyledItem}>
+          <PublishedComponent
+            pubRef='core.DatePicker'
+            module='policyHolder'
+            label='dateValidFrom'
+            value={this._filterValue('dateValidFrom')}
+            onChange={(v) =>
+              this._onChangeDateFilter(
+                'dateValidFrom',
+                v,
+                GREATER_OR_EQUAL_LOOKUP,
+              )
             }
-        ])
-    }
-
-    render() {
-        const { intl, filters, onChangeFilters } = this.props;
-        return (
-            <Grid container component={StyledForm}>
-                <Grid size={2} component={StyledItem}>
-                    <TextInput
-                        module="policyHolder"
-                        label="code"
-                        value={this._filterTextFieldValue('code')}
-                        onChange={v => this._onChangeStringFilter('code', v, CONTAINS_LOOKUP)}
-                    />
-                </Grid>
-                <Grid size={2} component={StyledItem}>
-                    <TextInput
-                        module="policyHolder"
-                        label="tradeName"
-                        value={this._filterTextFieldValue('tradeName')}
-                        onChange={v => this._onChangeStringFilter('tradeName', v, CONTAINS_LOOKUP)}
-                    />
-                </Grid>
-                <Grid size={8}>
-                    <PublishedComponent
-                        pubRef="location.DetailedLocationFilter"
-                        withNull={true}
-                        filters={filters}
-                        onChangeFilters={onChangeFilters}
-                        anchor="parentLocation"
-                    />
-                </Grid>
-                <Grid size={3} component={StyledItem}>
-                    <PublishedComponent
-                        pubRef="policyHolder.LegalFormPicker"
-                        module="policyHolder"
-                        label="legalForm"
-                        withNull
-                        nullLabel={formatMessage(intl, "policyHolder", "any")}
-                        value={this._filterValue('legalForm')}
-                        onChange={v => this._onChangeFilter('legalForm', v)}
-                    />
-                </Grid>
-                <Grid size={3} component={StyledItem}>
-                    <PublishedComponent
-                        pubRef="policyHolder.ActivityCodePicker"
-                        module="policyHolder"
-                        label="activityCode"
-                        withNull
-                        nullLabel={formatMessage(intl, "policyHolder", "any")}
-                        value={this._filterValue('activityCode')}
-                        onChange={v => this._onChangeFilter('activityCode', v)}
-                    />
-                </Grid>
-                <Grid size={2} component={StyledItem}>
-                    <PublishedComponent
-                        pubRef="core.DatePicker"
-                        module="policyHolder"
-                        label="dateValidFrom"
-                        value={this._filterValue('dateValidFrom')}
-                        onChange={v => this._onChangeDateFilter('dateValidFrom', v, GREATER_OR_EQUAL_LOOKUP)}
-                    />
-                </Grid>
-                <Grid size={2} component={StyledItem}>
-                    <PublishedComponent
-                        pubRef="core.DatePicker"
-                        module="policyHolder"
-                        label="dateValidTo"
-                        value={this._filterValue('dateValidTo')}
-                        onChange={v => this._onChangeDateFilter('dateValidTo', v, LESS_OR_EQUAL_LOOKUP)}
-                    />
-                </Grid>
-                <Grid size={2} component={StyledItem}>
-                    <FormControlLabel
-                        control={<Checkbox 
-                            checked={!!this._filterValue('isDeleted')}
-                            onChange={event => this._onChangeFilter('isDeleted', event.target.checked)}
-                            name="isDeleted" 
-                        />}
-                        label={formatMessage(intl, "policyHolder", "isDeleted")}
-                    />
-                </Grid>
-            </Grid>
-        )
-    }
+          />
+        </Grid>
+        <Grid size={3} component={StyledItem}>
+          <PublishedComponent
+            pubRef='core.DatePicker'
+            module='policyHolder'
+            label='dateValidTo'
+            value={this._filterValue('dateValidTo')}
+            onChange={(v) =>
+              this._onChangeDateFilter('dateValidTo', v, LESS_OR_EQUAL_LOOKUP)
+            }
+          />
+        </Grid>
+        <Grid size={3} component={StyledItem}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={!!this._filterValue('isDeleted')}
+                onChange={(event) =>
+                  this._onChangeFilter('isDeleted', event.target.checked)
+                }
+                name='isDeleted'
+              />
+            }
+            label={formatMessage(intl, 'policyHolder', 'isDeleted')}
+          />
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export { StyledForm };
